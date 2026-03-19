c6441e4c6fc9db2cb3c83a46dad573e67a19111e
Ningcong Chen <nchen3@wpi.edu>
2026-03-18 22:09:23 -0400

    update template

--- interactive_run.py
+++ interactive_run.py
@@ -13,24 +13,26 @@ print(HOME)
 parser = argparse.ArgumentParser(
                     prog='sinteractive',
                     description='Run interactive job on slurm cluster easily')
-parser.add_argument('--config', type=str, help="JSON configuration file for running job")
-parser.add_argument('--gpu', type=int, help="Number of gpu, default to 4") 
-parser.add_argument('--generate', default=False, action='store_true', help="Generate command for running")
-parser.add_argument('--nodelist', default=None, help="Run on a special nodelist")
+parser.add_argument('--config', default=None, type=str, help="JSON configuration file for running job")
+parser.add_argument('--template', default=False, action="store_true", help="Generate json template")
+parser.add_argument('--test', default=False, action='store_true', help="Generate command for debugging, but not running")
 parser.add_argument('--port', default=None, help="Open a port, must specified to avoid conflict")
 parser.add_argument('--cleanup', action="store_true", help="Regenerate all the keys")
+
 args = parser.parse_args()
+if args.template:
+    print("Generate template")
+    with open(f"{PWD}/template.json") as f:
+        print(f.read())
+    exit(0)
+if args.config is None:
+    print("--config is required!")
+    exit(1)
 PORT = args.port
 if PORT is None:
     # hope that we are lucky!
     print("WARNING : randomly open a port!")
     PORT = randrange(3000, 8000)
-
-if args.config is None:
-    if args.gpu == 0:
-        args.config = f"{PWD}/cpu_default.json"
-    else:
-        args.config = f"{PWD}/gpu_default.json"
     
 """
 Execute a script in python
@@ -75,8 +77,6 @@ REQMEM=config["REQMEM"]
 REQTIME=config["REQTIME"]
 PARTITION=config["PARTITION"]
 REQGPU=config["REQGPU"]
-if args.gpu is not None:
-    REQGPU=args.gpu
 
 cmd = f"""
 srun -N 1 \
@@ -87,13 +87,15 @@ srun -N 1 \
 --partition={PARTITION} \
 --gres=gpu:{REQGPU} \
 """
-if args.nodelist is not None:
-    cmd += f"--nodelist={args.nodelist }"
+
+if "nodelist" in config:
+    cmd += f""" --nodelist={config["nodelist"]} """
 
 if REQGPU != 0:
     REQTYP=config["REQTYP"]
-    cmd += f"--constraint={REQTYP}"
+    cmd += f" --constraint={REQTYP} "
+
 cmd += f"  bash {PWD}/sshd_script_new.sh {PWD} {PORT} {HOME}"
 print(cmd)
-if not args.generate:
+if not args.test:
     run_cmd_string(cmd, is_async=True)
